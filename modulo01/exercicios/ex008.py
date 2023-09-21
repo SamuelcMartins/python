2e5cb22bb7f17ad5a9da2a78cc670b1c6cf98d7f
SamuelcMartins <samuelcarmo.m05@gmail.com>
2023-09-21 11:27:31 -0300

    Create ex008.py

---
+++ modulo01/exercicios/ex008.py
@@ -0,0 +1,8 @@
+m = float(input('uma distância em metros: '))
+print(f'''A medida de {m}m corresponde a
+{m / 1000}km
+{m / 100}hm
+{m / 10}dam
+{m * 10:.0f}dm
+{m * 100:.0f}cm
+{m * 1000:.0f}mm''')
